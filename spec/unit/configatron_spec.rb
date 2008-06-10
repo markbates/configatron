70bd02457688e24279fb380509ba7fb2cfcfad56
Mark Bates <mark@markbates.com>
2008-06-10 12:49:08 -0400

    More specs.

--- spec/unit/configatron_spec.rb
+++ spec/unit/configatron_spec.rb
@@ -1,19 +1,25 @@
 require 'pathname'
 require Pathname(__FILE__).dirname.expand_path.parent + 'spec_helper'
 
-describe "In Kernel" do
+describe "The configuration method" do
   
-  it "there should be a configatron method" do
+  it "should be available in the Kernel" do
     configatron
   end
+  
+  it "should return a parameter using the parameter name as a method call"
+  
+  it "should return nil using the parameter name as a method call if the parameter doesn't exist"
+  
+  it "should take a block that allows for the configuration of the configatron system"
   
 end
 
 describe Configatron do
   
   it "should be a Singleton" do
-    lambda {Configatron.new}.should raise_error(NoMethodError)
-    Configatron.instance.should equal Configatron.instance
+    lambda{Configatron.new}.should raise_error(NoMethodError)
+    Configatron.instance.should == Configatron.instance
   end
   
 end
