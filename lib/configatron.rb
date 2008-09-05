76e18408c564a81f1d984e829b99822d2038ba02
Josh Nichols <josh@technicalpickles.com>
2008-09-05 14:53:07 -0400

    Move configuration, helpers, kernel, store, and yaml_store into subdirectory

--- lib/configatron.rb
+++ lib/configatron.rb
@@ -1,10 +1,11 @@
 require 'singleton'
 require 'yaml'
-require File.join(File.dirname(__FILE__), 'helpers')
-require File.join(File.dirname(__FILE__), 'kernel')
-require File.join(File.dirname(__FILE__), 'configuration')
-require File.join(File.dirname(__FILE__), 'store')
-require File.join(File.dirname(__FILE__), 'yaml_store')
+
+require File.join(File.dirname(__FILE__), 'configatron', 'helpers')
+require File.join(File.dirname(__FILE__), 'configatron', 'kernel')
+require File.join(File.dirname(__FILE__), 'configatron', 'configuration')
+require File.join(File.dirname(__FILE__), 'configatron', 'store')
+require File.join(File.dirname(__FILE__), 'configatron', 'yaml_store')
 
 module Configatron # :nodoc:
 end
