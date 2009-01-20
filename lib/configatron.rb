61b423e6d41fe9e13b3cab610c209e4a42b2cd13
Rob Sanheim <rsanheim@gmail.com>
2009-01-20 15:35:51 -0500

    oops, this got moved

--- lib/configatron.rb
+++ lib/configatron.rb
@@ -1,7 +1,6 @@
 base = File.join(File.dirname(__FILE__), 'configatron')
 require 'yaml'
 require 'erb'
-require File.join(base, 'kernel')
 require File.join(base, 'configatron')
 require File.join(base, 'store')
 require File.join(base, 'errors')
