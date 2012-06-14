3421acb98d7ddd34a3f6b4fc5a543d504668cb38
Michael Phan-Ba <michael@mikepb.com>
2012-06-14 14:45:17 -0700

    require yecht for jruby

--- lib/configatron/store.rb
+++ lib/configatron/store.rb
@@ -1,7 +1,9 @@
 class Configatron
   class Store
     if RUBY_VERSION.match(/^1\.9\.[^1]/)
-      unless RUBY_PLATFORM == 'java'
+      if RUBY_PLATFORM == 'java'
+        require 'yecht'
+      else
         require 'syck'
         ::YAML::ENGINE.yamler = 'syck'
       end
