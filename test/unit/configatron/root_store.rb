a290d3a5093e1e2505b1b8bdfb45b0e22db68bb7
Greg Brockman <gdb@gregbrockman.com>
2014-06-10 08:52:21 -0700

    Change Kernel -> Root

--- test/unit/configatron/root_store.rb
+++ test/unit/configatron/root_store.rb
@@ -1,14 +1,14 @@
 require_relative '../_lib'
 
-class Critic::Unit::KernelTest < Critic::Unit::Test
+class Critic::Unit::RootTest < Critic::Unit::Test
   before do
-    @kernel = Configatron::RootStore.new
+    @root = Configatron::RootStore.new
   end
 
   describe 'delegation' do
     it 'passes on to Configatron::Store' do
-      @kernel.a.b.c.d = 'D'
-      assert_equal('D', @kernel.a.b.c.d)
+      @root.a.b.c.d = 'D'
+      assert_equal('D', @root.a.b.c.d)
     end
   end
 
