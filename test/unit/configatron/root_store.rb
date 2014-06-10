7d242508ebea70269d5b2a4aad5fd6dc92440938
Greg Brockman <gdb@gregbrockman.com>
2014-06-10 08:54:57 -0700

    Delegate to_s and inspect

--- test/unit/configatron/root_store.rb
+++ test/unit/configatron/root_store.rb
@@ -17,4 +17,21 @@ class Critic::Unit::RootTest < Critic::Unit::Test
       assert_equal(true, configatron.kind_of?(Configatron::RootStore))
     end
   end
+
+  describe 'inspect' do
+    it 'delegates to store' do
+      @root.a.b.c.d = 'D'
+      @root.a.b.e = 'E'
+      assert_equal('configatron.a.b.c.d = "D"
+configatron.a.b.e = "E"', @root.inspect)
+    end
+  end
+
+  describe 'to_s' do
+    it 'delegates to store' do
+      @root.a.b.c.d = 'D'
+      @root.a.b = 'D'
+      assert_equal('configatron', @root.to_s)
+    end
+  end
 end
